42fcf5c3c90f8226a101dd326d9d3495c57d75a8
Dayoon Lee <dayoon273@gmail.com>
2021-01-28 22:02:35 +0900

    chapter3_exercise

---
+++ MATLAB/Chapter3/chater3_exercise.m
@@ -0,0 +1,36 @@
+%3
+% name = input('What is the name of a chemical element? ', 's')
+% n_len = length(name);
+% disp(n_len)
+
+%4
+%vec = input('Enter a vector: ')
+
+%5
+% var = input('Enter a real number: ');
+% fprintf('The number is %.2f\n', var)
+
+%6
+% x = 12345.6789;
+% fprintf('x is %f\n', x)
+% fprintf('x is %10.4f\n', x)
+% fprintf('x is %10.2f\n', x)
+% fprintf('x is %6.4f\n', x)
+% fprintf('x is %2.4f\n', x)
+
+%7
+% y = 12345;
+% fprintf('y is %d\n', y)
+% fprintf('y is %5d\n', y)
+% fprintf('y is %8d\n', y)
+% fprintf('y is %3d\n', y)
+
+%8
+% frin = input('Enter the flow in m^3/s: ');
+% flowrate(frin);
+
+%9
+% in = input('Enter your string: ', 's');
+% echostring(in);
+
+%10
