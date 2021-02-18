2d8cd97920099b81678a8c6dfe5f94dd1be815a9
Dayoon Lee <dayoon273@gmail.com>
2021-02-18 16:47:43 +0900

    Update Linear_regression.R

--- R/Linear_regression.R
+++ R/Linear_regression.R
@@ -2,6 +2,8 @@ library(MASS)
 library(ISLR)
 fix(Boston)
 ? Boston
+
+data(Boston)
 attach(Boston)
 
 lm.fit <- lm(medv~lstat)
@@ -12,6 +14,24 @@ lm.fit$coefficients
 predict(lm.fit, data.frame(lstat=c(5,10,15)))
 plot(lstat, medv)
 abline(lm.fit)
+abline(lm.fit, lwd=3)
+abline(lm.fit, lwd=3, col="red")
+plot(lstat, medv, pch=20)
+plot(lstat, medv, pch='+')
+plot(1:20, 1:20, pch=1:20)
 
-lm.fit <- lm(medv~., data=Boston)
+#multiple linear regression model
+lm.fit <- lm(medv~lstat+age, data=Boston)
 lm.fit
+
+lm.fit <- lm(medv~., data = Boston)
+lm.fit
+
+# Qualitative variables
+#ShelveLoc: qualitative variable (Bad, Medium, Good)
+#R generates dummy variables automatically
+fix(Carseats)
+lm.fit <- lm(Sales~., data=Carseats)
+lm.fit
+attach(Carseats)
+contrasts(ShelveLoc)
